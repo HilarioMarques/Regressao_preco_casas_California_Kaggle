74f16dc521af153c7e237cd6df48bfc301b3b9df
Hilario Filho <hilariospol@gmail.com>
2026-08-27 00:03:30 -0300

    commit automático - 2026-08-26 23:48:27

--- notebooks/src/config.py
+++ notebooks/src/config.py
@@ -5,7 +5,7 @@ PASTA_PROJETO = Path(__file__).resolve().parents[2]
 
 PASTA_DADOS = PASTA_PROJETO / "dados"
 
-# coloque abaixo o caminho para os arquivos de dados de seu projeto
+# abaixo o caminho para os arquivos de dados do projeto
 DADOS_ORIGINAIS = PASTA_DADOS / "housing.csv.zip"
 DADOS_LIMPOS = PASTA_DADOS / "housing_clean.parquet"
 DADOS_GEO_ORIGINAIS = PASTA_DADOS / "california_counties.geojson"
