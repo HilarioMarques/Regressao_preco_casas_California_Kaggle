3105d9abcfe56b690d6ca65547446515cf254bc5
Hilario Filho <hilariospol@gmail.com>
2026-09-15 02:34:33 -0300

    commit automático - 2026-09-14 10:17:25

--- home.py
+++ home.py
@@ -14,9 +14,12 @@ def carregar_dados_limpos():
 def carregar_dados_geo():
     return gpd.read_parquet(DADOS_GEO_MEDIAN)
 
-
+@st.cache_resource
 def carregar_modelo():
     return load(MODELO_FINAL)
 
+df = carregar_dados_limpos()
+gdf_geo = carregar_dados_geo()
+modelo = carregar_modelo()
 
 st.title("Previsão de preços de imóveis na California(USA)")
