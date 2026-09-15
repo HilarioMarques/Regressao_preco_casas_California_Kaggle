96cb564ea96565e2418b58a9fc9063efd59fa229
Hilario Filho <hilariospol@gmail.com>
2026-09-15 02:24:27 -0300

    commit automático - 2026-09-14 10:17:25

--- home.py
+++ home.py
@@ -1,5 +1,9 @@
 import geopandas as gpd
 import numpy as np
+import pandas as pd
 import streamlit as st
+
+from joblib import load
+from notebooks.src.config import DADOS_GEO_MEDIAN, DADOS_LIMPOS, MODELO_FINAL
 
 st.title("Previsão de preços de imóveis na California(USA)")
