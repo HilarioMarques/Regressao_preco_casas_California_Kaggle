779dc01b54b95fb3bd85a90ca9146a477928dab0
Hilario Filho <hilariospol@gmail.com>
2026-09-20 19:47:34 -0300

    commit automático - 2026-09-20 18:42:15

--- home.py
+++ home.py
@@ -57,53 +57,56 @@ condados = sorted(gdf_geo["name"].unique())
 coluna1, coluna2 = st.columns(2)
 
 with coluna1:
-    selecionar_condado = st.selectbox("Condado", condados)
 
-    longitude = gdf_geo.query("name == @selecionar_condado")["longitude"].values
-    latitude = gdf_geo.query("name == @selecionar_condado")["latitude"].values
+    with st.form(key="formulario"):
 
-    housing_median_age = st.number_input("Idade do imóvel", value=10, min_value=1, max_value=50)
+        selecionar_condado = st.selectbox("Condado", condados)
 
-    total_rooms = gdf_geo.query("name == @selecionar_condado")["total_rooms"].values
-    total_bedrooms = gdf_geo.query("name == @selecionar_condado")["total_bedrooms"].values
-    population = gdf_geo.query("name == @selecionar_condado")["population"].values
-    households = gdf_geo.query("name == @selecionar_condado")["households"].values
+        longitude = gdf_geo.query("name == @selecionar_condado")["longitude"].values
+        latitude = gdf_geo.query("name == @selecionar_condado")["latitude"].values
+
+        housing_median_age = st.number_input("Idade do imóvel", value=10, min_value=1, max_value=50)
+
+        total_rooms = gdf_geo.query("name == @selecionar_condado")["total_rooms"].values
+        total_bedrooms = gdf_geo.query("name == @selecionar_condado")["total_bedrooms"].values
+        population = gdf_geo.query("name == @selecionar_condado")["population"].values
+        households = gdf_geo.query("name == @selecionar_condado")["households"].values
 
 
-    median_income = st.slider("Renda média (milhares de US$)", 5.0, 100.0, 45.0, 5.0)
+        median_income = st.slider("Renda média (milhares de US$)", 5.0, 100.0, 45.0, 5.0)
 
-    ocean_proximity = gdf_geo.query("name == @selecionar_condado")["ocean_proximity"].values
+        ocean_proximity = gdf_geo.query("name == @selecionar_condado")["ocean_proximity"].values
 
-    bins_income = [0, 1.5, 3, 4.5, 6, np.inf]
-    median_income_cat = np.digitize(median_income/10, bins=bins_income)
+        bins_income = [0, 1.5, 3, 4.5, 6, np.inf]
+        median_income_cat = np.digitize(median_income/10, bins=bins_income)
 
-    room_per_households = gdf_geo.query("name == @selecionar_condado")["room_per_households"].values
-    bedrooms_per_room = gdf_geo.query("name == @selecionar_condado")["bedrooms_per_room"].values
-    population_per_households = gdf_geo.query("name == @selecionar_condado")["population_per_households"].values
+        room_per_households = gdf_geo.query("name == @selecionar_condado")["room_per_households"].values
+        bedrooms_per_room = gdf_geo.query("name == @selecionar_condado")["bedrooms_per_room"].values
+        population_per_households = gdf_geo.query("name == @selecionar_condado")["population_per_households"].values
 
-    entrada_modelo = {
-        "longitude": longitude,
-        "latitude":latitude,
-        "housing_median_age":housing_median_age,
-        "total_rooms":total_rooms,
-        "total_bedrooms": total_bedrooms,
-        "population":population,
-        "households": households,
-        "median_income": median_income/10,
-        "ocean_proximity":ocean_proximity,
-        "median_income_cat":median_income_cat,
-        "room_per_households":room_per_households,
-        "bedrooms_per_room":bedrooms_per_room,
-        "population_per_households":population_per_households,
-    }
+        entrada_modelo = {
+            "longitude": longitude,
+            "latitude":latitude,
+            "housing_median_age":housing_median_age,
+            "total_rooms":total_rooms,
+            "total_bedrooms": total_bedrooms,
+            "population":population,
+            "households": households,
+            "median_income": median_income/10,
+            "ocean_proximity":ocean_proximity,
+            "median_income_cat":median_income_cat,
+            "room_per_households":room_per_households,
+            "bedrooms_per_room":bedrooms_per_room,
+            "population_per_households":population_per_households,
+        }
 
-    df_entrada_modelo = pd.DataFrame(entrada_modelo)
+        df_entrada_modelo = pd.DataFrame(entrada_modelo)
 
-    botao_previsao = st.button("Prever Preço")
+        botao_previsao = st.form_submit_button("Prever Preço")
 
-    if botao_previsao:
-        preco = modelo.predict(df_entrada_modelo)
-        st.write(f"Preço previsto: US$ {preco[0][0]:.2f}")
+        if botao_previsao:
+            preco = modelo.predict(df_entrada_modelo)
+            st.write(f"Preço previsto: US$ {preco[0][0]:.2f}")
 
 with coluna2:
 
